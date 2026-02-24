73fa3fd4a773dcf823c33e839010bd50ab35b73c
monil <kansagaramonil@gmail.com>
2026-02-24 11:54:54 +0530

    Updated project structure and fixed gitignore

--- obj/Debug/net8.0/Backend_API.AssemblyInfo.cs
+++ obj/Debug/net8.0/Backend_API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7a4af5dd0afa280abf63c3eb77ac4d35b444bcf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
